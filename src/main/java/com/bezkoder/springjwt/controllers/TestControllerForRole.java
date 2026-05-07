ddbd2fdd0dfd3b266bee2979f99e6487dad4de0b
unknown <bilwesh.xworkz@gmail.com>
2026-05-07 16:05:37 +0530

    Update Profile (Update Email/password)

--- src/main/java/com/bezkoder/springjwt/controllers/TestControllerForRole.java
+++ src/main/java/com/bezkoder/springjwt/controllers/TestControllerForRole.java
@@ -1,11 +1,29 @@
 package com.bezkoder.springjwt.controllers;
 
+import com.bezkoder.springjwt.models.User;
+import com.bezkoder.springjwt.payload.request.UpdateProfileRequest;
+import com.bezkoder.springjwt.repository.UserRepository;
+import com.bezkoder.springjwt.security.services.UserDetailsImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestControllerForRole {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @GetMapping("/api/user/test")
     @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public String userAccess() {
@@ -18,5 +36,25 @@ public class TestControllerForRole {
     public String adminAccess() {
         System.out.println("Admin Access...");
         return "Admin Content";
+    }
+
+    @PutMapping("/api/user/update")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    public ResponseEntity<?> UpdateProfile(@RequestBody UpdateProfileRequest request) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+        User user = userRepository.findById(userDetails.getId())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        user.setEmail(request.getEmail());
+
+        if(request.getPassword() != null && ! request.getPassword().isEmpty()){
+            user.setPassword(passwordEncoder.encode(request.getPassword()));
+        }
+
+        userRepository.save(user);
+        return ResponseEntity.ok("Profile updated Successfully");
     }
 }
---
+++ src/main/java/com/bezkoder/springjwt/payload/request/UpdateProfileRequest.java
@@ -0,0 +1,24 @@
+package com.bezkoder.springjwt.payload.request;
+
+
+
+public class UpdateProfileRequest {
+    public String email;
+    public String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
